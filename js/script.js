15900ffbe6247a16604bd17876d303deefe08c27
Artur Mochnia <amochnia@gmail.com>
2023-05-26 14:13:19 +0100

    format kod html in script

--- js/script.js
+++ js/script.js
@@ -56,22 +56,18 @@
 
     for (const task of tasks) {
       htmlString += `
-            <li
-              class="tasks__item ${
-                task.done && hideDoneTasks ? "tasks__item--hidden" : ""
-              } js-task">
-            <button class="tasks__button tasks__button--toggleDone js-toggleDone${
-              task.done ? " emoji " : ""
-            }">
-            </button>
-            <span class="tasks__content${
-              task.done ? " tasks__content--toggleDone " : ""
-            }">${task.content}</span>
-           <button class="tasks__button tasks__button--remove js-remove">
-             🗑
-           </button>
-            </li>
-            `;
+      <li class="tasks__item ${
+        task.done && hideDoneTasks ? "tasks__item--hidden" : ""
+      } js-task">
+        <button class="tasks__button tasks__button--toggleDone js-toggleDone${
+          task.done ? " emoji " : ""
+      }"></button>
+      <span class="tasks__content${task.done ? " tasks__content--toggleDone " : ""}">
+        ${task.content}
+      </span>
+     <button class="tasks__button tasks__button--remove js-remove">🗑</button>
+      </li>
+    `;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
